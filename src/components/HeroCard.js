0a93c0c5846984de28dcb27b9603fdd9eacb3f42
Terence <terence223@mac.com>
2020-05-24 23:49:31 +0800

    fetch data for HeroCards

---
+++ src/components/HeroCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroCard() {
+    return (
+        <div>
+            HeroCard
+        </div>
+    );
+}
+
+export default HeroCard;
--- src/pages/Heroes.js
+++ src/pages/Heroes.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
+
 import HeroProfile from '../components/HeroProfile';
+import HeroCard from '../components/HeroCard';
 
 function Heroes() {
 
     let { path, url } = useRouteMatch();
 
+    const [heros, setHeros] = useState([]);    // 所有英雄基本資料
+
+    // 取得所有英雄基本資料
+    const fetchHeros = () => {
+        axios.get('https://hahow-recruit.herokuapp.com/heroes')
+            .then((res) => {
+                setHeros(res.data);
+            })
+            .catch((error) => {
+            });
+    }
+    
+    useEffect(() => {
+        fetchHeros();
+    }, []);
+
     return (
         <div>
+            {
+                heros.map(hero => <HeroCard key={hero.id} heroData={hero} />)
+            }
             <Switch>
                 <Route path={`${path}/:heroId`}>
                     <HeroProfile />
