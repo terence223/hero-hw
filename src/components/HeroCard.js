d57769095ac661e3cba6241f1358ace928900f2f
Terence <terence223@mac.com>
2020-05-25 02:52:44 +0800

    fix some router problem

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -9,16 +9,13 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import {Grid, Card, CardActionArea, CardMedia, CardContent, Typography} from '@material-ui/core';
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 function HeroCard({ heroData }) {
-
-    let { heroId } = useParams();
-
     return (
         <GridCol item xs={6} sm={4} md={4} lg={3}>
-            <Link to={`/heroes/${heroData.id}`}>
+            <TheLink to={`/heroes/${heroData.id}`}>
                 <Card>
                     <CardActionArea>
                         <CardMedia
@@ -28,12 +25,12 @@ function HeroCard({ heroData }) {
                         />
                         <CardContent>
                             <CardTitle gutterBottom variant="h5" component="h2">
-                                { Number(heroId) === heroData.id ? <SelectedName>{ heroData.name }</SelectedName> : <span>{ heroData.name }</span> }
+                                Lizard
                             </CardTitle>
                         </CardContent>
                     </CardActionArea>
                 </Card>
-            </Link>
+            </TheLink>
         </GridCol>
     );
 }
@@ -41,6 +38,10 @@ function HeroCard({ heroData }) {
 HeroCard.propTypes = {
     heroData: PropTypes.object.isRequired,
 };
+
+const TheLink = styled(Link)`
+    text-decoration: none;
+`;
 
 const SelectedName = styled.span`
     color: #E3325A;
